bcb98ab3a64dcf77c542092676941e854e8a19fa
ikrammmmm <ikramhussein1234@gmail.com>
2021-06-08 22:20:33 +0530

    updated link target

--- src/components/templates/portfolio.js
+++ src/components/templates/portfolio.js
@@ -5,8 +5,8 @@ export default class Portfolio extends Component {
     render()
     {
         const project = this.props.project;
-        var github = project.github ? <a href={project.github}>Github</a> : "";
-        var demo = project.demo ? <a href={project.demo}>Demo </a> : "";
+        var github = project.github ? <a href={project.github} target="_blank">Github</a> : "";
+        var demo = project.demo ? <a href={project.demo} target="_blank">Demo </a> : "";
         return (
             <div className="col col-6 card portfolio-card d-flex justify-content-center" >
             <div className="card-header text-center portfoilio-card-header ">
