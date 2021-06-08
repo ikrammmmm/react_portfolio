93fc0482fab219210e47b6772d6ba93359a92498
ikrammmmm <ikramhussein1234@gmail.com>
2021-06-08 19:41:27 +0530

    updated about us and readme

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -16,7 +16,12 @@ export class About extends Component
                         <div className="col-7  about-bg ">
                                <center> <h2>Ikram husain</h2></center>
                                 <div className="row about-content p-4">
-                                Hi my name is ikram i'm 23 years old. I love nature and knitting in my spare time. I volunteer for mental health and domestic violence support group. I born and raised in the same neighbourhood.
+                                <p>Hi my name is ikram i'm 23 years old. 
+                                I am a coding bootcamp student and aspire to become a web developer. I was exposed to web development from a friend and have been intrigued ever since. technology is ever evolving, and pushes us to learn everyday.
+                               </p><p>
+                               I love nature and knitting in my spare time.
+                                 I volunteer for mental health and domestic violence support group. 
+                                 I born and raised in the same neighbourhood.</p>
 
                                 </div>
                         </div>
