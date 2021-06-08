9f96235835874e94be5b3f7c3a6f52d11f4c59d5
ikrammmmm <ikramhussein1234@gmail.com>
2021-06-08 20:22:01 +0530

    updated projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,42 +3,43 @@ import burger from "../images/burger.png";
 import employee from "../images/employee.png";
 import weather from "../images/weather.png";
 import note from "../images/note.png";
+import googlebooks from "../images/google-book.png";
 
 var projects = [
     {
-        "name":"Weather App",
+        "name":"Stormy Weather App",
         "github":"https://github.com/ikrammmmm/Stromy",
         "demo": "https://ikrammmmm.github.io/Stromy/",
         "image":weather,
-        "Description" : ""
+        "Description" : "Weather app for the travelers to check weather in diffrent cities "
     },
     {
         "name":"Note Taker App",
         "github":"https://github.com/ikrammmmm/NoteTaker",
-        "demo": null,
+        "demo": "https://note-taker-apz.herokuapp.com/notes",
         "image":note,
-        "Description" : ""
+        "Description" : "Web app to write write and save the notes for the small business owners"
     },
     {
         "name":"Employee Directory App",
         "github":"https://github.com/ikrammmmm/employee_directory",
         "demo": "http://ikrammmmm.github.io/employee_directory",
         "image":employee,
-        "Description" : ""
+        "Description" : "Employee directory with React to view  entire employee directory at once and to sort and filter the list"
+    },
+    {
+        "name":"Google Books",
+        "github":"https://github.com/ikrammmmm/google-books",
+        "demo": "https://google-book-search-ikram.herokuapp.com/",
+        "image":googlebooks,
+        "Description" : " React-based Google Books Search app."
     },
     {
         "name":"Eat a Burger App",
         "github":"https://github.com/ikrammmmm/burger",
         "demo": "https://eat-burger-app-ikramm.herokuapp.com/",
         "image":burger,
-        "Description" : ""
-    },
-    {
-        "name":"Eat a Burger App",
-        "github":"https://github.com/ikrammmmm/burger",
-        "demo": "https://eat-burger-app-ikramm.herokuapp.com/",
-        "image":burger,
-        "Description" : ""
+        "Description" : "his is a burger logger app with MySQL, Node, Express, Handlebars and a homemade ORM."
     },
     {
         "name":"Eat a Burger App",
