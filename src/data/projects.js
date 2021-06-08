b9d35fb9b89ffe92e784521ef2d57109e57ce0ee
ikrammmmm <ikramhussein1234@gmail.com>
2021-06-08 20:27:58 +0530

    updated description error

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,42 +11,42 @@ var projects = [
         "github":"https://github.com/ikrammmmm/Stromy",
         "demo": "https://ikrammmmm.github.io/Stromy/",
         "image":weather,
-        "Description" : "Weather app for the travelers to check weather in diffrent cities "
+        "description" : "Weather app for the travelers to check weather in diffrent cities "
     },
     {
         "name":"Note Taker App",
         "github":"https://github.com/ikrammmmm/NoteTaker",
         "demo": "https://note-taker-apz.herokuapp.com/notes",
         "image":note,
-        "Description" : "Web app to write write and save the notes for the small business owners"
+        "description" : "Web app to write write and save the notes for the small business owners"
     },
     {
         "name":"Employee Directory App",
         "github":"https://github.com/ikrammmmm/employee_directory",
         "demo": "http://ikrammmmm.github.io/employee_directory",
         "image":employee,
-        "Description" : "Employee directory with React to view  entire employee directory at once and to sort and filter the list"
+        "description" : "Employee directory with React to view  entire employee directory at once and to sort and filter the list"
     },
     {
         "name":"Google Books",
         "github":"https://github.com/ikrammmmm/google-books",
         "demo": "https://google-book-search-ikram.herokuapp.com/",
         "image":googlebooks,
-        "Description" : " React-based Google Books Search app."
+        "description" : " React-based Google Books Search app."
     },
     {
         "name":"Eat a Burger App",
         "github":"https://github.com/ikrammmmm/burger",
         "demo": "https://eat-burger-app-ikramm.herokuapp.com/",
         "image":burger,
-        "Description" : "This is a burger logger app with MySQL, Node, Express, Handlebars and a homemade ORM."
+        "description" : "This is a burger logger app with MySQL, Node, Express, Handlebars and a homemade ORM."
     },
     {
         "name":"Eat a Burger App",
         "github":"https://github.com/ikrammmmm/burger",
         "demo": "https://eat-burger-app-ikramm.herokuapp.com/",
         "image":burger,
-        "Description" : ""
+        "description" : ""
     },
 
 ]
